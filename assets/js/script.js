5fe25841f994423d8694c794e8173c2bb59bc8f3
Karen Zeng <karen.zeng@outlook.com>
2023-08-02 22:54:03 -0400

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,7 @@ const overlay = document.querySelector("[data-overlay]");
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
+const modalDate = document.querySelector("[data-modal-date]");
 
 // modal toggle function
 const testimonialsModalFunc = function () {
@@ -42,6 +43,11 @@ for (let i = 0; i < testimonialsItem.length; i++) {
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    // Get the date value from the data-testimonials-date attribute and display it in the modal
+    const testimonialDate = this.dataset.testimonialsDate;
+    modalDate.textContent = new Date(testimonialDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
 
     testimonialsModalFunc();
 
@@ -157,3 +163,144 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+
+
+
+// Function to open the PDF modal for Project 1
+function openPdfModal1() {
+  const modal = document.getElementById('pdfModal1');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 1
+function closePdfModal1() {
+  const modal = document.getElementById('pdfModal1');
+  modal.style.display = 'none';
+}
+
+// Function to open the PDF modal for Project 2
+function openPdfModal2() {
+  const modal = document.getElementById('pdfModal2');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 2
+function closePdfModal2() {
+  const modal = document.getElementById('pdfModal2');
+  modal.style.display = 'none';
+}
+
+// Function to open the PDF modal for Project 3
+function openPdfModal3() {
+  const modal = document.getElementById('pdfModal3');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 3
+function closePdfModal3() {
+  const modal = document.getElementById('pdfModal3');
+  modal.style.display = 'none';
+}
+
+// Function to open the PDF modal for Project 4
+function openPdfModal4() {
+  const modal = document.getElementById('pdfModal4');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 4
+function closePdfModal4() {
+  const modal = document.getElementById('pdfModal4');
+  modal.style.display = 'none';
+}
+
+// Function to open the PDF modal for Project 5
+function openPdfModal5() {
+  const modal = document.getElementById('pdfModal5');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 5
+function closePdfModal5() {
+  const modal = document.getElementById('pdfModal5');
+  modal.style.display = 'none';
+}
+
+// Function to open the PDF modal for Project 6
+function openPdfModal6() {
+  const modal = document.getElementById('pdfModal6');
+  modal.style.display = 'block';
+}
+
+// Function to close the PDF modal for Project 6
+function closePdfModal6() {
+  const modal = document.getElementById('pdfModal6');
+  modal.style.display = 'none';
+}
+
+// Add event listener to open the PDF modal for Project 1 when the corresponding card is clicked
+const projectCard1 = document.querySelectorAll('.project-item a')[0];
+projectCard1.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal1();
+});
+
+// Add event listener to close the PDF modal for Project 1 when the close button is clicked
+const closeBtn1 = document.getElementById('closePdfModal1');
+closeBtn1.addEventListener('click', closePdfModal1);
+
+// Add event listener to open the PDF modal for Project 2 when the corresponding card is clicked
+const projectCard2 = document.querySelectorAll('.project-item a')[1];
+projectCard2.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal2();
+});
+
+// Add event listener to close the PDF modal for Project 2 when the close button is clicked
+const closeBtn2 = document.getElementById('closePdfModal2');
+closeBtn2.addEventListener('click', closePdfModal2);
+
+// Add event listener to open the PDF modal for Project 3 when the corresponding card is clicked
+const projectCard3 = document.querySelectorAll('.project-item a')[2];
+projectCard3.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal3();
+});
+
+// Add event listener to close the PDF modal for Project 3 when the close button is clicked
+const closeBtn3 = document.getElementById('closePdfModal3');
+closeBtn3.addEventListener('click', closePdfModal3);
+
+// Add event listener to open the PDF modal for Project 4 when the corresponding card is clicked
+const projectCard4 = document.querySelectorAll('.project-item a')[3];
+projectCard4.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal4();
+});
+
+// Add event listener to close the PDF modal for Project 4 when the close button is clicked
+const closeBtn4 = document.getElementById('closePdfModal4');
+closeBtn4.addEventListener('click', closePdfModal4);
+
+// Add event listener to open the PDF modal for Project 5 when the corresponding card is clicked
+const projectCard5 = document.querySelectorAll('.project-item a')[4];
+projectCard5.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal5();
+});
+
+// Add event listener to close the PDF modal for Project 5 when the close button is clicked
+const closeBtn5 = document.getElementById('closePdfModal5');
+closeBtn5.addEventListener('click', closePdfModal5);
+
+// Add event listener to open the PDF modal for Project 6 when the corresponding card is clicked
+const projectCard6 = document.querySelectorAll('.project-item a')[5];
+projectCard6.addEventListener('click', function(event) {
+  event.preventDefault();
+  openPdfModal6();
+});
+
+// Add event listener to close the PDF modal for Project 6 when the close button is clicked
+const closeBtn6 = document.getElementById('closePdfModal6');
+closeBtn6.addEventListener('click', closePdfModal6);
